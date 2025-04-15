8a7d52f726c0e8fdbc80be45f7f86c02ab44b3ee
Ssonamy <lev.salnikoff@icloud.com>
2025-04-15 22:53:13 +0300

    Добавил код для сравнения методов сортитровки (выполнил первую лабу)

--- firstLR/firstLR.cpp
+++ firstLR/firstLR.cpp
@@ -3,35 +3,116 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <cstdlib> // для rand()
+#include <ctime> // для time()
+#include <chrono> // для измерения времени
 #include "functions.h"
 
 using namespace std;
+
+
+// Функция для генерации и заполнения массива необходимой длинны случайными числами
+vector<int> generateRandomArray(int size)
+{
+	vector<int> arr(size);
+	for (int i = 0; i < size; ++i)
+		arr[i] = rand() % 10000;
+	return arr;
+}
+
+
+void bubbleSort(vector<int>& arr) // Сортировка пузырьком: проходит по массиву и меняет местами соседние элементы, если они идут в неправильном порядке. Повторяет это, пока массив не отсортирован.
+{
+	int size = arr.size();
+	for (int i = 0; i < size - 1; ++i) {
+		for (int j = 0; j < size - i - 1; ++j) {
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+		}
+	}
+}
+
+
+void insertionSort(vector<int>& arr) // Сортировка вставками: берет элементы один за другим и вставляет их в нужное место среди уже отсортированных элементов слева.
+{
+	int size = arr.size();
+	for (int i = 1; i < size; ++i) {
+		int key = arr[i];
+		int j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+void merge(vector<int>& arr, int left, int mid, int right) //Вспомогательная функция для сортироовки слиянием
+{
+	int i = left, j = mid + 1;
+	vector<int> temp;
+
+	while (i <= mid && j <= right) {
+		if (arr[i] < arr[j])
+			temp.push_back(arr[i++]);
+		else
+			temp.push_back(arr[j++]);
+	}
+
+	while (i <= mid) temp.push_back(arr[i++]);
+	while (j <= right) temp.push_back(arr[j++]);
+
+	for (int k = 0; k < temp.size(); ++k)
+		arr[left + k] = temp[k];
+}
+
+void mergeSort(vector<int>& arr, int left, int right) // Сортировка слиянием: делит массив на две части, сортирует каждую часть отдельно (рекурсивно), а потом сливает их обратно в один отсортированный массив.
+{
+	if (left < right) {
+		int mid = (left + right) / 2;
+		mergeSort(arr, left, mid);
+		mergeSort(arr, mid + 1, right);
+		merge(arr, left, mid, right);
+	}
+}
+
+template<typename Func>
+long long measureTime(Func sortFunction, vector<int> arr) {
+	auto start = chrono::high_resolution_clock::now();
+	sortFunction(arr);
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+	return duration.count();
+}
 
 int firstLR()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	srand(time(nullptr)); // Инициализация генератора случайных чисел
 
-	char switch_on;
+	vector<int> sizes = { 100, 1000, 10000 }; // Размеры массивов
 
-	cout << "_______________________________________\n";
-	cout << "Добро пожаловать в программу сортировки.\n";
-	cout << "Введите:\n";
-	cout << "1. Сортировка пузырьком.\n";
-	cout << "2. Сортировка вставками.\n";
-	cout << "2. Сортировка слиянием.\n";
-	cout << "_______________________________________\n";
-	cout << "Введите:";
-	cin >> switch_on;
+	for (int size : sizes) {
+		cout << "\nРазмер массива: " << size << "\n";
 
-	//switch (switch_on)
-	//{
-	//case 1:
-	//	cout << "все норм";
-	//	break;
-	//default:
-	//	cout << "ты лох";
-	//	break;
-	//}
+		auto base = generateRandomArray(size);
+
+		// Bubble Sort
+		long long t1 = measureTime(bubbleSort, base);
+		cout << "Bubble Sort: " << t1 << " мс\n";
+
+		// Insertion Sort
+		long long t2 = measureTime(insertionSort, base);
+		cout << "Insertion Sort: " << t2 << " мс\n";
+
+		// Merge Sort (через лямбду)
+		auto mergeWrapper = [&](vector<int> a) {
+			mergeSort(a, 0, a.size() - 1);
+			};
+		long long t3 = measureTime(mergeWrapper, base);
+		cout << "Merge Sort: " << t3 << " мс\n" << endl;
+	}
+
 	return 0;
 }
