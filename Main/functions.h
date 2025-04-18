295352321613bc80f9a217ec82141feac4b44d86
Ssonamy <lev.salnikoff@icloud.com>
2025-04-18 21:53:54 +0300

    Добавил весь код, остается фиксить баги и править код

--- Main/functions.h
+++ Main/functions.h
@@ -7,3 +7,19 @@ bool isInt(string x);
 int stringToInt(string x);
 
 int autoInput();
+
+int ZeroLaba();
+
+int FirstLaba();
+
+int SecondLaba();
+
+int ThirdLaba();
+
+int FourthLaba();
+
+int FifthLaba();
+
+int SixthLaba();
+
+int SeventhLaba();
